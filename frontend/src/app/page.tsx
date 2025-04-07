46f1a7bc6741140ad5eaf395eee4ba27694cecc4
sweet-salt <dudgh0314@naver.com>
2025-04-07 19:41:25 +0900

    style: 새로운 일정 html

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'; // 이 줄을 추가하세요
+
 import Nav from './componets/nav';
+
 export default function Home() {
+  function moveTrip() {
+    window.location.href = '/newtrip';
+  }
+  
   return (
     <>
-      <div>반갑습니다</div>
+      <div>
+        <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded" 
+          onClick={() => moveTrip()}
+        >
+          Go to Trip Detail
+        </button>
+      </div>
       <Nav />
     </>
   );
-  
 }
