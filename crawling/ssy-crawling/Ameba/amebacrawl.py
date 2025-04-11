dd672eb28a3495b0fc23cfe21c0a84af7a3bafd1
성시연 <demons@dgu.ac.kr>
2025-04-11 15:17:14 +0900

    feat:ssy-ameba 1page 완료

--- crawling/ssy-crawling/Ameba/amebacrawl.py
+++ crawling/ssy-crawling/Ameba/amebacrawl.py
@@ -12,7 +12,6 @@ import time
 import pickle
 import os
 import random
-
 from selenium.webdriver.chrome.options import Options
 
 
@@ -26,4 +25,72 @@ driver_path = r"D:\webdriver\chromedriver-win64\chromedriver-win64\chromedriver.
 service = Service(driver_path)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(f"https://search.ameba.jp/general/{find}.html")
+
+time.sleep(3)
+
+
+try:
+    driver.find_element(By.XPATH, '//*[@id="page"]/main/div[3]/div[1]/section[2]/div[3]/a').click()
+except Exception as e:
+    print(f"Login failed: {e}")
+
+print("클래스들")
+page_classes = driver.find_elements(By.XPATH, "//*[@class]")
+for element in page_classes[:5]:  # 처음 5개 요소만 확인
+    print(f"Element: {element.tag_name}, Classes: {element.get_attribute('class')}")
+
+
+
+try:
+    for p in range(1, 11):
+        post = driver.find_elements(By.XPATH, f'//*[@id="page"]/main/div[3]/div[1]/section/ul/li[{p}]')
+        
+        for item in post:
+            # 제목 추출
+            try:
+                title_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_EntryTitle')
+                title = title_element.text.strip()
+            except NoSuchElementException:
+                title = ''
+            
+            # 내용 추출
+            try:
+                data_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_EntryData')
+                data = data_element.text.strip()
+            except NoSuchElementException:
+                data = ''
+            
+            # 글 요약 추출
+            try:
+                content_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_EntryContent')
+                content = content_element.text.strip()
+            except NoSuchElementException:
+                content = ''
+        
+            # 링크 추출 (일반적으로 <a> 태그 안에 href 속성이 있음)
+            try:
+                link_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_Link')
+                link = link_element.get_attribute('href')
+
+                link_element.click()
+                time.sleep(3)
+                
+            except NoSuchElementException:
+                link = ''
+                
+            
+            # 추출한 내용 출력
+            print("제목:", title)
+            print("내용:", data)
+            print("글 요약:", content)
+            print("링크:", link)
+            print("-" * 40)
+
+            driver.back()
+            break
+        
+except NoSuchElementException:
+    print("PcEntryList_List 컨테이너를 찾을 수 없습니다.")
+
+driver.implicitly_wait(10)
 
