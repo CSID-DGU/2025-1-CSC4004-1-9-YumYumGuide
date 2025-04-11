701171c76387768090808f8ea757e5dda3fe84fe
성시연 <demons@dgu.ac.kr>
2025-04-11 13:14:55 +0900

    feat:ssy-초기설정

---
+++ crawling/ssy-crawling/Blog/blogcrawl.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+import pandas as pd
+import numpy as np
+import time
+import pickle
+import os
+import random
+
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+find = "도쿄"
+
+driver_path = r"D:\webdriver\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+service = Service(driver_path)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(f"https://section.blog.naver.com/Search/Post.naver?pageNo=1&rangeType=ALL&orderBy=sim&keyword={find}")
