4057b0727422c5e20d2ad412638c146d8ca2eff9
성시연 <demons@dgu.ac.kr>
2025-05-03 17:55:37 +0900

    feat:ssy-ameba 3page까지 오류 없이 완료

--- crawling/ssy-crawling/Ameba/amebacrawl.py
+++ crawling/ssy-crawling/Ameba/amebacrawl.py
@@ -19,7 +19,6 @@ find = "東京"
 driver_path = r"D:\webdriver\chromedriver-win64\chromedriver-win64\chromedriver.exe"
 service = Service(driver_path)
 driver = webdriver.Chrome(options=chrome_options)
-driver.get(f"https://search.ameba.jp/general/{find}.html")
 
 time.sleep(3)
 
@@ -27,56 +26,74 @@ with open('ameba.csv', 'w', newline='', encoding='utf-8-sig') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['제목', '내용', '글 요약', '링크'])
 
-try:
-    driver.find_element(By.XPATH, '//*[@id="page"]/main/div[3]/div[1]/section[2]/div[3]/a').click()
-except Exception as e:
-    print(f"Login failed: {e}")
-# for page in range(1, 101):
 
+for page in range(1, 101):
+    driver.get(f"https://search.ameba.jp/general/entry/{find}.html?p={page}")
+    try:
+        driver.find_element(By.XPATH, '//*[@id="page"]/main/div[3]/div[1]/section/div[1]/ul/li[2]').click()
+    except Exception as e:
+        print(f"Login failed: {e}")
+    try:
+        for p in range(1, 11):
+            post = driver.find_elements(By.XPATH, f'//*[@id="page"]/main/div[3]/div[1]/section/ul/li[{p}]')
+            print(p)
+            for item in post:
+                # 제목 추출
+                try:
+                    title_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_EntryTitle')
+                    title = title_element.text.strip()
+                except NoSuchElementException:
+                    title = ''
+            
+                # 링크 추출 (일반적으로 <a> 태그 안에 href 속성이 있음)
+                try:
+                    link_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_Link')
+                    link = link_element.get_attribute('href')
 
-try:
-    for p in range(1, 11):
-        post = driver.find_elements(By.XPATH, f'//*[@id="page"]/main/div[3]/div[1]/section/ul/li[{p}]')
-        
-        for item in post:
-            # 제목 추출
-            try:
-                title_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_EntryTitle')
-                title = title_element.text.strip()
-            except NoSuchElementException:
-                title = ''
-        
-            # 링크 추출 (일반적으로 <a> 태그 안에 href 속성이 있음)
-            try:
-                link_element = item.find_element(By.CLASS_NAME, 'PcEntryListItem_Link')
-                link = link_element.get_attribute('href')
+                except NoSuchElementException:
+                    link = ''
+                    theme = ""
+                    content = ''
+
 
                 if link:
                     driver.execute_script("window.open('', '_blank');")
                     driver.switch_to.window(driver.window_handles[1])
                     driver.get(link)
                     time.sleep(3)
+                    
+                    try:
+                        theme = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/article/div/div[1]/div[2]/dl').text.strip()
+                        content = driver.find_element(By.XPATH, '//*[@id="entryBody"]').text.strip()
+                    except NoSuchElementException:
+                        theme = ''
+                        content = ''
 
-                theme = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/article/div/div[1]/div[2]/dl').text
-                content = driver.find_element(By.XPATH, '//*[@id="entryBody"]').text
+                    with open('ameba.csv', 'a', newline='', encoding='utf-8-sig') as csv_file:
+                        csv_writer = csv.writer(csv_file)
+                        csv_writer.writerow([title, theme, content])
 
-                print(theme)
-                print(content)
 
-                time.sleep(3)
+                # 번역 나중에 하기!!!
+                # for attempt in range(3):
+                #     try:
+                #         # 번역 요청
+                #         result = translator.translate(theme, src='a', dest='ko')
+                #     except Exception as e:
+                #         print(f"번역 오류 (시도 {attempt+1}/{3}): {e}")
                 
-            except NoSuchElementException:
-                link = ''
-        
-            with open('ameba.csv', 'a', newline='', encoding='utf-8-sig') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow([title, theme, content])
+                driver.close()
+                link = None
+                driver.switch_to.window(driver.window_handles[0])
 
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
-            time.sleep(1)
-            break
-        
-except NoSuchElementException:
-    print("PcEntryList_List 컨테이너를 찾을 수 없습니다.")
+                print("테마 : ", theme)
+                print("내용: ", content)
+                time.sleep(3)
+            
+        driver.get(f"https://search.ameba.jp/general/entry/{find}.html?p={page}")
 
+    except NoSuchElementException:
+        print("PcEntryList_List 컨테이너를 찾을 수 없습니다.")
+
+
+driver.close()
