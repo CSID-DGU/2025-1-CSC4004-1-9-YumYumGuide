8ec7e4f8b25857bd87f5fa6144c594d0b58ce132
박지현 <wlgusqkr11@naver.com>
2025-05-22 14:41:29 +0900

    feat:편의점 상세보기, 편의점 가게별로 데이터 받는 api 추가

--- backend/src/convenience/convenience.service.ts
+++ backend/src/convenience/convenience.service.ts
@@ -1,19 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { CreateConvenienceDto } from './dto/create-convenience.dto';
 import { UpdateConvenienceDto } from './dto/update-convenience.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Convenience, ConvenienceDocument } from './schema/convenience.schema';
+import { Model } from 'mongoose';
+import { ApiResponseDto, PaginationResponseDto } from 'src/common/dto/api.response.dto';
+import { ConvenienceCategory } from './enums/convenience-category.enum';
+import { GetConvenienceDto } from './dto/get-conveneience.dto';
 
 @Injectable()
 export class ConvenienceService {
-  create(createConvenienceDto: CreateConvenienceDto) {
-    return 'This action adds a new convenience';
+  constructor(
+    @InjectModel(Convenience.name) private readonly convenienceModel: Model<ConvenienceDocument>,
+  ) {
+
   }
 
-  findAll() {
-    return `This action returns all convenience`;
+  async findOneGoods(_id: string) {
+    const goods = await this.convenienceModel.findOne({ _id });
+
+    if (!goods) {
+      return new ApiResponseDto(false, 404, 'not found', null);
+    }
+
+    return new ApiResponseDto(true, 200, 'success', goods);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} convenience`;
+  async findOneConvenienceAllGoods(dto: GetConvenienceDto) {
+    const { category, page, take } = dto;
+    const skip = (page - 1) * take;
+
+    const [items, totalCount] = await Promise.all([
+      this.convenienceModel
+        .find({ category })
+        .skip(skip)
+        .limit(take)
+        .exec(),
+      this.convenienceModel.countDocuments({ category })
+    ]);
+
+    if (!items.length) {
+      return new ApiResponseDto(false, 404, 'not found', null);
+    }
+
+    const paginatedData = new PaginationResponseDto(items, totalCount, page, take);
+    return new ApiResponseDto(true, 200, 'success', paginatedData);
   }
 
   update(id: number, updateConvenienceDto: UpdateConvenienceDto) {
