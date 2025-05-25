d5bc65b51b628f01b83c02f78e9eeb849a9096bb
beomaura <junbeom@dgu.ac.kr>
2025-05-25 22:19:15 +0900

    feat: 유저, 개인기호 스키마 정의

--- backend/src/favorite/schema/favorite.schema.ts
+++ backend/src/favorite/schema/favorite.schema.ts
@@ -4,16 +4,38 @@ import { Document } from 'mongoose';
 
 export type FavoriteDocument = Favorite & Document;
 
-@Schema()
+@Schema({ timestamps: true })
 export class Favorite {
 
-  @Prop({ required: true })
-  startDate: Date;
+  @Prop({ required: true, unique: true })
+  userId: string;
 
-  @Prop({ required: true })
-  endDate: Date;
+  @Prop({ enum: ['예', '아니오'], required: true })
+  smoking: string;
+
+  @Prop({ enum: ['예', '아니오'], required: true })
+  drinking: string;
+
+  @Prop({ enum: ['맛집 위주', '관광지 위주'], required: true })
+  travelStyle: string;
+
+  @Prop({ enum: ['육류', '해산물', '면류', '밥류'], required: true })
+  favoriteFood: string;
+
+  @Prop({
+    type: [String],
+    enum: ['견과류', '해산물', '유제품', '밀가루'],
+    default: [],
+    required: true
+  })
+  hateFood: string[];
+
+  @Prop({ enum: ['1인&2인', '3인이상'], required: true })
+  groupType: string;
+
+  @Prop({ enum: ['자연', '축제', '역사', '액티비티', '랜드마크'], required: true })
+  attractionType: string;
 
 }
 
 export const FavoriteSchema = SchemaFactory.createForClass(Favorite);
-
--- backend/src/user/schemas/user.schema.ts
+++ backend/src/user/schemas/user.schema.ts
@@ -13,8 +13,8 @@ export class User {
   @Prop({ required: true })
   email: string;
 
-  @Prop({ required: true })
-  password: string;
+  @Prop({ required: true, unique: true })
+  userId: string;
 
 }
 
